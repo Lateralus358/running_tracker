f0b70fc892c3845f7cbdbdb81fc9ecad21e7e29c
Luke Feeney <luke@Lukes-MacBook-Air.local>
2017-07-01 23:13:19 +0100

    updated css and html

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Tracker
 
 # Create your views here.
 def track_list(request):
-    return render(request, 'tracker/track_list.html', {})
+    tracking_data = Tracker.objects.filter(run_date__lte=timezone.now()).order_by('run_date')
+    return render(request, 'tracker/track_list.html', {'tracking_data' : tracking_data})
