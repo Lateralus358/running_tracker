5160f2d04db20dc04c080a98545eabedecefa634
Lateralus358 <lateralus358@gmail.com>
2017-07-02 20:41:15 +0000

    updated button for dash

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.track_list, name='track_list'),
+    url(r'^tracker/dashboard/$', views.tracker_dashboard, name='tracker_dashboard'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -4,5 +4,8 @@ from .models import Tracker
 
 # Create your views here.
 def track_list(request):
-    tracking_data = Tracker.objects.filter(run_date__lte=timezone.now()).order_by('run_date')
+    tracking_data = Tracker.objects.filter(run_date__lte=timezone.now()).order_by('-run_date')
     return render(request, 'tracker/track_list.html', {'tracking_data' : tracking_data})
+
+def tracker_dashboard(request):
+    return render(request, 'tracker/tracker_dash.html', {})
